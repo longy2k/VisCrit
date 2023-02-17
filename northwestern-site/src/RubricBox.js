bd08a48113c125fb369dd9d47239fccfa398a7da
Jeffrey Deng <124819735+jzdengeki@users.noreply.github.com>
2023-02-17 13:36:36 -0500

    Implemented a test CheckBox in Rubric

--- northwestern-site/src/RubricBox.js
+++ northwestern-site/src/RubricBox.js
@@ -7,6 +7,43 @@ export default function RubricBox(){
                 <li>Comment</li>
                 <li>Location</li>
             </ul>
+            <div className="categoryDiv">
+            <button className="categoryButton" onClick={ShowHideCategory} id="Toggle">-</button>
+            Category
+            <div id="categoryContent">
+                <ul className="content-list">
+                    <li>
+                        <div>
+                            <button className="checkBox" onClick={ShowHideComment} id="Mark">_</button> 
+                            Option 1
+                        </div>
+                    </li>
+                    <li>Comment TODO</li>
+                    <li>Location TODO</li>
+                </ul>
+            </div>
+            </div>
         </div>
     )
+}
+
+function ShowHideCategory() {
+    var x = document.getElementById("categoryContent");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+    var buttonText=document.getElementById("Toggle");
+    if (buttonText.innerText === "+"){
+        buttonText.innerText="-";
+    }
+    else {
+        buttonText.innerText="+";
+    }
+}
+
+function ShowHideComment(){
+    var x = document.getElementById("")
 }
