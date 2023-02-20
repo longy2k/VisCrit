3c06839c622f55b17264568292f99a0b821437de
Jeffrey Deng <124819735+jzdengeki@users.noreply.github.com>
2023-02-20 16:44:17 -0500

    unfinished Upload using react-drag-drop-files

--- northwestern_site/frontend/src/UploadSection.js
+++ northwestern_site/frontend/src/UploadSection.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React, {useState} from "react";
+import { FileUploader } from "react-drag-drop-files";
 
+const fileTypes = ["PDF"];
 export default function UploadSection(){
+    const [file, setFile] = useState(null);
+    const handleChange = (file) => {
+        setFile(file);
+    }
     return (
         <div className="uploadSection">
-            UPLOAD
+            <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
         </div>
     )
 }
