f228c653318230f6a768beb6caa2096e3d38267e
seyelence <infinityx14@gmail.com>
2023-02-24 17:54:30 -0500

    test

--- northwestern_site/northwestern_site/settings.py
+++ northwestern_site/northwestern_site/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'northwestern_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '<db_name>',
+        'USER': '<db_username>',
+        'PASSWORD': '<password>',
+        'HOST': '<db_hostname_or_ip>',
+        'PORT': '<db_port>',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
