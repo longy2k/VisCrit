7c06e875831fa01a31343e3dc6da261f0a348663
Christos016 <124848353+Christos016@users.noreply.github.com>
2023-02-24 19:54:02 -0500

    Update Home.js

--- northwestern_site/frontend/src/components/Home.js
+++ northwestern_site/frontend/src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
     The Slide Critique site is for users to upload a slide, dashboard, and other related
     ideas to be critiqued. The users who upload the slide are able to send invites through
     e-mail to their recipients. This site makes getting feedback from a large group of people
-    very easy.
+    very easy ok great.
     </p>
     </div>
     );
