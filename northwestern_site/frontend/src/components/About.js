681251d7d30f24e71cc0706fcb587cd1add574ec
Jeffrey Deng <124819735+jzdengeki@users.noreply.github.com>
2023-02-26 00:40:03 -0500

    Got the data extraction mostly working, still need to fix the nested Hierachies.
    The input button in Rubric section is there for Testing and should stay there until no further testing is needed, so don't remove it. Other than that, I still need to work on the comment section

--- northwestern_site/frontend/src/components/About.js
+++ northwestern_site/frontend/src/components/About.js
@@ -1,13 +1,40 @@
-import React from "react"
+import React, {useState} from "react"
+import XLSX from "xlsx"
+import Hierarchy from "./Dropdown_Gen"
 
-function About() {
+export default function About() {
+    const[jsonData, setData] = useState([]);
+    const handleFile = async(e) => {
+        const file = e.target.files[0];
+        const data = await file.arrayBuffer();
+        const workbook = XLSX.readFile(data);
+        const worksheet = workbook.Sheets[workbook.SheetNames[3]];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet, {header: 1, blankrows:false})
+        setData(jsonData)
+    }
+    
+    const Hierarchies= [];
+    const HierarchyMap = new Map();
+    console.log(jsonData);
+    for(let data in jsonData){
+        if(data == 0){
+            continue;
+        }
+        if(HierarchyMap.has(jsonData[data][2])){
+            HierarchyMap.get(jsonData[data][2]).addItem(jsonData[data]);
+        }
+        else{
+            HierarchyMap.set(jsonData[data][2], new Hierarchy(jsonData[data], false));
+            Hierarchies.push(HierarchyMap.get(jsonData[data][2]));
+        }
+    }
+    console.log(Hierarchies) 
+    console.log(HierarchyMap.size) 
     return (
-    <div class= "home">
-    <h2> Background </h2>
-    <p> 
-placeholder
-    </p>
-    </div>
-    );
+        <div>
+            <input type="file" onChange={(e) => handleFile(e)}/>
+            {Hierarchies.map((item, i) => 
+            <div key={i}>{item.returnHTML()}</div>)}
+        </div>
+    )
 }
-export default About;
