dd85587267a9ce470a466c31b1cc2f8bfbbd8c6e
Christos016 <124848353+Christos016@users.noreply.github.com>
2023-02-26 20:34:39 -0500

    Add files via upload
    home page

--- northwestern_site/frontend/src/components/Home.js
+++ northwestern_site/frontend/src/components/Home.js
@@ -1,16 +1,113 @@
-import React from "react"
+import React from "react";
 
-function Home() {
-    return (
+
+
+
+
+function Home(){
+  const mystyle={
+    color: "black",
+    
+    padding: "10px",
+    fontFamily: "side"
+
+  }
+ 
+
+
+  return(
     <div class= "home">
-    <h2> Welcome </h2>
-    <p> 
-    The Slide Critique site is for users to upload a slide, dashboard, and other related
-    ideas to be critiqued. The users who upload the slide are able to send invites through
-    e-mail to their recipients. This site makes getting feedback from a large group of people
-    very easy.
-    </p>
+      <h1 style={{
+        display:"flex",
+        justifyContent: "center",
+
+      
+      }}> Welcome to VisCrit</h1>
+      <div style={
+        {
+          margin: '1px',
+          border: '2px solid gray'
+        }
+      }></div>
+
+
+    
+    <div id="Borderor">
+      
+      <ul className="borl">
+        
+        
+        <div style={{
+          display: "flex",
+          justifyContent: "center"
+        }}> The Slide Critique site is for users to upload a slide, dashboard, and other related
+            ideas to be critiqued. The users who upload the slide are able to send invites through
+            e-mail to their recipients. This site makes getting feedback from a large group of people
+            very easy. </div>
+      </ul>
     </div>
-    );
+
+   
+    <div id="Bordero">
+      <ul className="borr">
+        
+        <div style={{
+          display: "flex",
+          justifyContent: "center"
+          
+        }}> Instructions: </div>
+       good instructions go here:
+      </ul>
+      
+    </div>
+    
+
+    
+    
+   
+    
+  
+    
+    </div>
+    
+  );
 }
-export default Home;
+
+
+
+
+ export default Home;
+
+
+// function Home() {
+//     return (
+//         <div class= "home">
+//     <h2> Welcome to VisCrit </h2>
+    
+    
+//     <p> 
+//     The Slide Critique site is for users to upload a slide, dashboard, and other related
+//     ideas to be critiqued. The users who upload the slide are able to send invites through
+//     e-mail to their recipients. This site makes getting feedback from a large group of people
+//     very easy.
+    
+    
+//     </p>
+    
+    
+//     </div>
+   
+  
+    
+
+     
+//     );
+    
+
+
+    
+    
+    
+// }
+// export default Home;
+
