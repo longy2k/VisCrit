bc06b8e69e016388cc442825154bb9bd6cad0c6b
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-02 18:11:38 -0500

    Removed repeating rubric-field

--- client/src/components/RubricSection.js
+++ client/src/components/RubricSection.js
@@ -10,7 +10,7 @@ export default function RubricSection(){
       const value = event.target.value;
       setInputs(values => ({...values, [name]: value}))
     }
-  
+
     const handleSubmit = (event) => {
       event.preventDefault();
       alert(inputs);
@@ -18,85 +18,16 @@ export default function RubricSection(){
 
     return (
         <div className="rubricSection">
-        <form onSubmit={handleSubmit}>  
+        <form onSubmit={handleSubmit}>
         <div>
             <label className="rubric-field"> Category 1
-            <input 
-                type="text" 
-                name="category1" 
-                defaultValue={inputs.category || ""} 
+            <input
+                type="text"
+                name="category1"
+                defaultValue={inputs.category || ""}
                 onChange={handleChange}/>
             </label>
         </div>
-
-        <div>
-            <label> Category 2
-            <input 
-                type="text" 
-                name="category2" 
-                defaultValue={inputs.category || ""} 
-                onChange={handleChange}/>
-            </label>
-        </div>
-
-        <div className="rubric-field">
-            <label>Category 3
-            <input 
-                type="text" 
-                name="category3"
-                defaultValue={inputs.category || ""} 
-                onChange={handleChange}
-            />
-            </label>
-        </div>
-          
-        <div className="rubric-field">
-            <label>Category 4
-            <input 
-                type="text" 
-                name="category4" 
-                defaultValue={inputs.category || ""} 
-                onChange={handleChange}/>
-            </label>
-        </div>
-
-        <div className="rubric-field">
-            <label>Category 5
-            <input 
-                type="text" 
-                name="category5" 
-                defaultValue={inputs.category || ""} 
-                onChange={handleChange}/>
-          </label>
-        </div>
-
-        <div className="rubric-field">
-            <label>Category 6
-            <input 
-                type="text" 
-                name="category6" 
-                defaultValue={inputs.category || ""} 
-                onChange={handleChange}/>
-          </label>
-        </div>
-
-        <div className="rubric-field">
-            <label> Category 7
-            <input 
-                type="text" 
-                name="category7" 
-                defaultValue={inputs.category || ""} 
-                onChange={handleChange}/>
-          </label>
-        </div>
-
-        <label> Category 8
-            <input 
-                type="text" 
-                name="category8" 
-                defaultValue={inputs.category || ""} 
-                onChange={handleChange}/>
-        </label>
           <br></br>
         <input type="submit" />
       </form>
