477a01ca5eed03f67de31a5e0d882e59cb8339d3
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-02 18:16:49 -0500

    Removed .uploadSection and .rubricSection from style.css

--- client/src/components/UploadP.js
+++ client/src/components/UploadP.js
@@ -19,6 +19,7 @@ export default function UploadP(){
 
     return (
         <div className="Upload-Page">
+            <h1>Create</h1>
             <UploadSection />
             <RubricSection />
         </div>
