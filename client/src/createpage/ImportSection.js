9ea6820c685b8cffd79fabfc707d04726eb98705
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-11 21:51:04 -0500

    Download xlsx to json from CreatePage

--- client/src/createpage/ImportSection.js
+++ client/src/createpage/ImportSection.js
@@ -1,29 +1,40 @@
 import React, {useState} from "react";
 import Data_Extractor from "../userpage/Data_Extract";
 import * as XLSX from 'xlsx';
+import axios from 'axios';
 
 export default function RubricSection(){
 
   const [jsonData, setJsonData] = useState(null);
 
-  const readUploadFile = (e) => {
-      e.preventDefault();
-      if (e.target.files) {
-          const reader = new FileReader();
-          reader.onload = (e) => {
-              const data = e.target.result;
-              const workbook = XLSX.read(data, { type: "array" });
-              const sheetName = workbook.SheetNames[0];
-              const worksheet = workbook.Sheets[sheetName];
-              const json = XLSX.utils.sheet_to_json(worksheet);
-              setJsonData(json);
-              console.log(json);
-          };
-          reader.readAsArrayBuffer(e.target.files[0]);
-          const Hierarchy = Data_Extractor(jsonData);
-      }
-  }
+  const readUploadFile = async (e) => {
+    e.preventDefault();
+    const files = e.currentTarget.files;
+    if (files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: "array" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = XLSX.utils.sheet_to_json(worksheet);
+        setJsonData(json);
+        console.log(json);
 
+        // Upload JSON file to server
+        const formData = new FormData();
+        formData.append('file', new Blob([JSON.stringify(json)], { type: 'application/json' }), file.name.replace(/\.[^/.]+$/, ".json"));
+        try {
+          const response = await axios.post('/api/upload', formData);
+          console.log(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
 
 
   return (
