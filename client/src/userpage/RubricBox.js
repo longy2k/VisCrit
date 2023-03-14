0be4ff0fcf1a7af40b70a5cf86fc13e012117a71
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-13 21:17:48 -0400

    Displaying recent json file from CreatePage to UserPage

--- client/src/userpage/RubricBox.js
+++ client/src/userpage/RubricBox.js
@@ -3,14 +3,14 @@ import { ItemContext } from "./ItemContext"
 
 export default function RubricBox(){
     let {Hierarchy} = useContext(ItemContext)
-    const [data, setData] = useState({});
+    const [jsonData, setData] = useState({});
 
     useEffect(() => {
       fetch('/api/upload/json')
         .then(response => response.json())
-        .then(data => {
-          console.log(data.path);
-          setData(data);
+        .then(jsonData => {
+          console.log(jsonData.path);
+          setData(jsonData);
         });
     }, []);
 
@@ -19,8 +19,9 @@ export default function RubricBox(){
               <ul className="toolBarMenu">
                   <li><strong>Available Categories</strong></li>
               </ul>
-              {Hierarchy.map((item, i) =>
-              <div key={i}>{item.returnHTML()}</div>)}
+              {Object.keys(jsonData).map((key) => (
+                  <li key={key}>{key}: {JSON.stringify(jsonData[key])}</li>
+              ))}
           </div>
     )
 }
