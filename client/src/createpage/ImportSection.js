48377c15f9cfe1ac8c96f80eec82ac7528601290
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-27 00:09:31 -0400

    Createpage showing import details

--- client/src/createpage/ImportSection.js
+++ client/src/createpage/ImportSection.js
@@ -57,20 +57,22 @@ export default function RubricSection(){
               {jsonData.map((item, index) => (
                 <div key={index}>
                   <div className="rubricBlock">
-                    <p>{item.RubricID}</p>
-                    <p>{item.CatLevel01}</p>
-                    <p>{item.CatLevel_Item}</p>
-                    <p>{item.CatLevel_Item_DisplayText}</p>
-                    <p>{item.CatLevel02}</p>
-                    <p>{item.CatLevel02_DefaultStateOpen}</p>
-                    <p>{item.CatLevel02_DisplayText}</p>
-                    <p>{item.CatLevel02_MouseOverText}</p>
-                    <p>{item.CatLevel_Item}</p>
-                    <p>{item.CatLevel_Item_DisplayText}</p>
-                    <p>{item.CatLevel_Item_MouseOverText}</p>
-                    <p>{item.HasLikert}</p>
-                    <p>{item.HasLocation}</p>
-                    <p>{item.MustEnterCritique}</p>
+                    <p>{Object.keys(item)[0]}: {item.RubricID}</p>
+                    <p>{Object.keys(item)[1]}: {item.RubricListingOrder}</p>
+                    <p>{Object.keys(item)[2]}: {item.CatLevel01}</p>
+                    <p>{Object.keys(item)[3]}: {item.CatLevel01_DefaultStateOpen}</p>
+                    <p>{Object.keys(item)[4]}: {item.CatLevel02}</p>
+                    <p>{Object.keys(item)[5]}: {item.CatLevel02_DefaultStateOpen}</p>
+                    <p>{Object.keys(item)[6]}: {item.CatLevel_Item}</p>
+                    <p>{Object.keys(item)[7]}: {item.CatLevel01_DisplayText}</p>
+                    <p>{Object.keys(item)[8]}: {item.CatLevel02_DisplayText}</p>
+                    <p>{Object.keys(item)[9]}: {item.CatLevel_Item_DisplayText}</p>
+                    <p>{Object.keys(item)[10]}: {item.CatLevel01_MouseOverText}</p>
+                    <p>{Object.keys(item)[11]}: {item.CatLevel02_MouseOverText}</p>
+                    <p>{Object.keys(item)[12]}: {item.CatLevel_Item_MouseOverText}</p>
+                    <p>{Object.keys(item)[13]}: {item.HasLocation}</p>
+                    <p>{Object.keys(item)[14]}: {item.HasLikert}</p>
+                    <p>{Object.keys(item)[15]}: {item.MustEnterCritique}</p>
                   </div>
                 </div>
               ))}
