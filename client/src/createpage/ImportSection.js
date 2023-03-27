69061f4e3dc06bf983d3b7145e91dc633ca53565
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-27 01:11:34 -0400

    Removed NULL and NA items

--- client/src/createpage/ImportSection.js
+++ client/src/createpage/ImportSection.js
@@ -22,7 +22,8 @@ export default function RubricSection(){
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const json = XLSX.utils.sheet_to_json(worksheet);
-        setJsonData(json);setFileUploaded(true);
+        setJsonData(json);
+        setFileUploaded(true);
         console.log(json);
 
         // Upload JSON file to server
@@ -57,28 +58,49 @@ export default function RubricSection(){
             />
         </form>
         <div className="json-container">
+          {/* {jsonData && (
+            <div>
+              {jsonData.map((item, index) => (
+                <div key={index}>
+                  {item && (
+                  <div className="rubricBlock">
+                      <p>{Object.keys(item)[0]}: {item.RubricID}</p>
+                      <p>{Object.keys(item)[1]}: {item.RubricListingOrder}</p>
+                      <p>{Object.keys(item)[2]}: {item.CatLevel01}</p>
+                      <p>{Object.keys(item)[3]}: {item.CatLevel01_DefaultStateOpen}</p>
+                      <p>{Object.keys(item)[4]}: {item.CatLevel02}</p>
+                      <p>{Object.keys(item)[5]}: {item.CatLevel02_DefaultStateOpen}</p>
+                      <p>{Object.keys(item)[6]}: {item.CatLevel_Item}</p>
+                      <p>{Object.keys(item)[7]}: {item.CatLevel01_DisplayText}</p>
+                      <p>{Object.keys(item)[8]}: {item.CatLevel02_DisplayText}</p>
+                      <p>{Object.keys(item)[9]}: {item.CatLevel_Item_DisplayText}</p>
+                      <p>{Object.keys(item)[10]}: {item.CatLevel01_MouseOverText}</p>
+                      <p>{Object.keys(item)[11]}: {item.CatLevel02_MouseOverText}</p>
+                      <p>{Object.keys(item)[12]}: {item.CatLevel_Item_MouseOverText}</p>
+                      <p>{Object.keys(item)[13]}: {item.HasLocation}</p>
+                      <p>{Object.keys(item)[14]}: {item.HasLikert}</p>
+                      <p>{Object.keys(item)[15]}: {item.MustEnterCritique}</p>
+                  </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          )} */}
           {jsonData && (
             <div>
               {jsonData.map((item, index) => (
                 <div key={index}>
-                  <div className="rubricBlock">
-                    <p>{Object.keys(item)[0]}: {item.RubricID}</p>
-                    <p>{Object.keys(item)[1]}: {item.RubricListingOrder}</p>
-                    <p>{Object.keys(item)[2]}: {item.CatLevel01}</p>
-                    <p>{Object.keys(item)[3]}: {item.CatLevel01_DefaultStateOpen}</p>
-                    <p>{Object.keys(item)[4]}: {item.CatLevel02}</p>
-                    <p>{Object.keys(item)[5]}: {item.CatLevel02_DefaultStateOpen}</p>
-                    <p>{Object.keys(item)[6]}: {item.CatLevel_Item}</p>
-                    <p>{Object.keys(item)[7]}: {item.CatLevel01_DisplayText}</p>
-                    <p>{Object.keys(item)[8]}: {item.CatLevel02_DisplayText}</p>
-                    <p>{Object.keys(item)[9]}: {item.CatLevel_Item_DisplayText}</p>
-                    <p>{Object.keys(item)[10]}: {item.CatLevel01_MouseOverText}</p>
-                    <p>{Object.keys(item)[11]}: {item.CatLevel02_MouseOverText}</p>
-                    <p>{Object.keys(item)[12]}: {item.CatLevel_Item_MouseOverText}</p>
-                    <p>{Object.keys(item)[13]}: {item.HasLocation}</p>
-                    <p>{Object.keys(item)[14]}: {item.HasLikert}</p>
-                    <p>{Object.keys(item)[15]}: {item.MustEnterCritique}</p>
-                  </div>
+                  {item && (
+                    <div className="rubricBlock">
+                      {Object.keys(item).map((key, i) => (
+                        item[key] !== "NULL" && item[key] !== "NA" && (
+                          <p key={i}>
+                            {key}: {item[key]}
+                          </p>
+                        )
+                      ))}
+                    </div>
+                  )}
                 </div>
               ))}
             </div>
