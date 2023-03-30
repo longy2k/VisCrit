acf8bec72f3021a30ef695abccaa09c683a358e8
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-30 19:35:23 -0400

    Implemented react-pdf

--- client/src/userpage/DocumentReader.js
+++ client/src/userpage/DocumentReader.js
@@ -1,9 +1,43 @@
+// import React, { useState, useEffect } from 'react';
+// import CommentB from './CritiqueBox';
+// import AnnotateScript from "./AnnotateScript";
+
+// export default function DocumentReader() {
+//   const [data, setData] = useState({});
+
+//   useEffect(() => {
+//     fetch('/api/upload/pdf')
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data.path);
+//         setData(data);
+//       });
+//   }, []);
+
+//   return (
+//     <div className="fileView">
+//     {data.path && <embed src={`http://localhost:5000/${data.path}`}
+//       type="application/pdf" width="100%" height="100%" />}
+//       <CommentB />
+//     </div>
+//   );
+// }
+
 import React, { useState, useEffect } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
 import CommentB from './CritiqueBox';
 import AnnotateScript from "./AnnotateScript";
 
+// Set the worker URL to the version bundled with react-pdf.
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 export default function DocumentReader() {
   const [data, setData] = useState({});
+  const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
 
   useEffect(() => {
     fetch('/api/upload/pdf')
@@ -16,8 +50,16 @@ export default function DocumentReader() {
 
   return (
     <div className="fileView">
-    {data.path && <embed src={`http://localhost:5000/${data.path}`}
-      type="application/pdf" width="100%" height="100%" />}
+      {data.path && (
+        <Document
+          file={`http://localhost:5000/${data.path}`}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+          ))}
+        </Document>
+      )}
       <CommentB />
     </div>
   );
