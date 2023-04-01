7188be2f7613aff9e8eea96efe9174947e45dfe3
Long Huynh <longhuynh.y2k@gmail.com>
2023-04-01 04:05:35 -0400

    cleared warnings

--- client/src/userpage/DocumentReader.js
+++ client/src/userpage/DocumentReader.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import CommentB from './CritiqueBox';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Set the worker URL to the version bundled with react-pdf.
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -19,6 +21,12 @@ export default function DocumentReader() {
     const context = canvas.getContext('2d'); // get the canvas context
     context.fillStyle = 'blue';
     context.fillRect(50, 600, 100, 50); // move the rectangle to (50, 400)
+    canvas.addEventListener('click', (event) => {
+      const x = event.offsetX; // get the x-coordinate of the cursor relative to the canvas
+      const y = event.offsetY; // get the y-coordinate of the cursor relative to the canvas
+      const imageData = context.getImageData(50, 600, 100, 50); // get the image data of the original rectangle
+      context.putImageData(imageData, x, y); // draw the image data at the cursor position
+    });
   }
 
   useEffect(() => {
