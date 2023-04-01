d3f4fdab66040b5f62256953eb4ef847b3cb16ae
Long Huynh <longhuynh.y2k@gmail.com>
2023-04-01 15:57:35 -0400

    Canvas overlay

--- client/src/userpage/DocumentReader.js
+++ client/src/userpage/DocumentReader.js
@@ -12,43 +12,89 @@ export default function DocumentReader() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
+  let rectangles = [];
+  let rectangle = null;
+  
   function onDocumentLoadSuccess({ numPages: nextNumPages }) {
     setNumPages(nextNumPages);
+  
+    const reactPdfPage = document.querySelector('.react-pdf__Page');
+    const canvas = document.createElement('canvas');
+    reactPdfPage.appendChild(canvas);
+    const context = canvas.getContext('2d');
+    let isDrawing = false;
+    let startX, startY;
+  
+    canvas.addEventListener('mousedown', (event) => {
+      isDrawing = true;
+      const rect = canvas.getBoundingClientRect();
+      startX = event.clientX - rect.left;
+      startY = event.clientY - rect.top;
+      rectangle = { x: startX, y: startY, width: 0, height: 0 };
+    });
+  
+    canvas.addEventListener('mousemove', (event) => {
+      if (isDrawing) {
+        const rect = canvas.getBoundingClientRect();
+        const currentX = event.clientX - rect.left;
+        const currentY = event.clientY - rect.top;
+        const width = currentX - startX;
+        const height = currentY - startY;
+        rectangle.width = width;
+        rectangle.height = height;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+  
+        // Draw existing rectangles
+        context.fillStyle = 'blue';
+        rectangles.forEach((rect) => {
+          context.fillRect(rect.x, rect.y, rect.width, rect.height);
+        });
+  
+        // Draw the current rectangle
+        context.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+      }
+    });
+  
+    canvas.addEventListener('mouseup', (event) => {
+      isDrawing = false;
+      rectangles.push(rectangle);
+      rectangle = null;
+    });
   }
   
-  let rectangles = []; // declare an empty array to store the rectangles
+  // let rectangles = []; // declare an empty array to store the rectangles
 
-function onRenderSuccess() {
-  const canvas = document.querySelector('.react-pdf__Page__canvas'); // get the canvas element
-  const context = canvas.getContext('2d'); // get the canvas context
-  let isDrawing = false;
-  let startX, startY;
+// function onRenderSuccess() {
+//   const canvas = document.querySelector('.react-pdf__Page__canvas'); // get the canvas element
+//   const context = canvas.getContext('2d'); // get the canvas context
+//   let isDrawing = false;
+//   let startX, startY;
 
-  canvas.addEventListener('mousedown', (event) => {
-    isDrawing = true;
-    startX = event.offsetX;
-    startY = event.offsetY;
-  });
+//   canvas.addEventListener('mousedown', (event) => {
+//     isDrawing = true;
+//     startX = event.offsetX;
+//     startY = event.offsetY;
+//   });
 
-  canvas.addEventListener('mousemove', (event) => {
-    if (isDrawing) {
-      // context.clearRect(0, 0, canvas.width, canvas.height);
-      const width = event.offsetX - startX;
-      const height = event.offsetY - startY;
-      context.fillStyle = 'blue';
-      context.fillRect(startX, startY, width, height);
-    }
-  });
+//   canvas.addEventListener('mousemove', (event) => {
+//     if (isDrawing) {
+//       // context.clearRect(0, 0, canvas.width, canvas.height);
+//       const width = event.offsetX - startX;
+//       const height = event.offsetY - startY;
+//       context.fillStyle = 'blue';
+//       context.fillRect(startX, startY, width, height);
+//     }
+//   });
 
-  canvas.addEventListener('mouseup', (event) => {
-    isDrawing = false;
-    const width = event.offsetX - startX;
-    const height = event.offsetY - startY;
-    const rectangle = { x: startX, y: startY, width, height };
-    rectangles.push(rectangle); // add the rectangle to the array
-    console.log(rectangles); // display the array in the console
-  });
-}
+//   canvas.addEventListener('mouseup', (event) => {
+//     isDrawing = false;
+//     const width = event.offsetX - startX;
+//     const height = event.offsetY - startY;
+//     const rectangle = { x: startX, y: startY, width, height };
+//     rectangles.push(rectangle); // add the rectangle to the array
+//     console.log(rectangles); // display the array in the console
+//   });
+// }
 
 
   useEffect(() => {
@@ -78,7 +124,8 @@ function onRenderSuccess() {
               onLoadSuccess={onDocumentLoadSuccess}
               renderMode="canvas"
             >
-              <Page pageNumber={pageNumber} onRenderSuccess={onRenderSuccess} />
+              {/* <Page pageNumber={pageNumber} onRenderSuccess={onRenderSuccess} /> */}
+              <Page pageNumber={pageNumber} />
             </Document>
           </>
         )}
