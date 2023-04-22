317f6f8bae82b052b214dcedabcc4db916fe17f9
Long Huynh <longhuynh.y2k@gmail.com>
2023-04-22 13:58:17 -0400

    Upload button loads multiple files

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -18,39 +18,56 @@ export default function NavBar() {
       if (files.length > 0) {
         const file = files[0];
         const reader = new FileReader();
-        reader.onload = async (e) => {
-          const data = e.target.result;
-          const workbook = XLSX.read(data, { type: "array" });
-          const sheetName = workbook.SheetNames[0];
-          const worksheet = workbook.Sheets[sheetName];
-          const json = XLSX.utils.sheet_to_json(worksheet);
-          setJsonData(json);
-          setFileUploaded(true);
-          console.log(json);
-
-          // Upload JSON file to server
+    
+        if (file.type === "application/vnd.ms-excel" || file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.type === "text/csv") {
+          reader.onload = async (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = XLSX.utils.sheet_to_json(worksheet);
+            setJsonData(json);
+            setFileUploaded(true);
+            console.log(json);
+    
+            // Upload JSON file to server
+            const formData = new FormData();
+            formData.append('file', new Blob([JSON.stringify(json, null, 2)], { type: 'application/json' }), file.name.replace(/\.[^/.]+$/, ".json"));
+            try {
+              const response = await axios.post('/api/upload/', formData);
+              console.log(response.data);
+            } catch (error) {
+              console.error(error);
+            }
+    
+            const participantsSheetName = workbook.SheetNames[3];
+            const participantsWorksheet = workbook.Sheets[participantsSheetName];
+            const jsonData = XLSX.utils.sheet_to_json(participantsWorksheet);
+            setParticipantsData(jsonData);
+          };
+          reader.readAsArrayBuffer(file);
+        } else if (file.type === "application/pdf" || file.type === "image/jpeg" || file.type === "image/png") {
+          // Upload file to server
           const formData = new FormData();
-          formData.append('file', new Blob([JSON.stringify(json, null, 2)], { type: 'application/json' }), file.name.replace(/\.[^/.]+$/, ".json"));
+          formData.append('file', file, file.name);
           try {
             const response = await axios.post('/api/upload/', formData);
             console.log(response.data);
           } catch (error) {
             console.error(error);
           }
-          
-          const participantsSheetName = workbook.SheetNames[3];
-          const participantsWorksheet = workbook.Sheets[participantsSheetName];
-          const jsonData = XLSX.utils.sheet_to_json(participantsWorksheet);
-          setParticipantsData(jsonData);
-        };
-        reader.readAsArrayBuffer(file);
+        } else {
+          console.error("Invalid file type.");
+        }
       }
     };
+    
 
     const handleUploadButtonClick = (e) => {
       const uploadInput = document.createElement('input');
       uploadInput.type = 'file';
-      uploadInput.accept = '.xlsx, .xls, .csv';
+      uploadInput.accept = '.xlsx, .xls, .csv, .pdf, .jpeg, .png';
+      uploadInput.multiple = true;
       uploadInput.onchange = readUploadFile;
       uploadInput.click();
     }
