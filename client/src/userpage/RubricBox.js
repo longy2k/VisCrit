d7646ff54533f6fc1f2e2543646683fb56303cab
Long Huynh <longhuynh.y2k@gmail.com>
2023-04-23 14:30:02 -0400

    Moved export button outside of rubricBox

--- client/src/userpage/RubricBox.js
+++ client/src/userpage/RubricBox.js
@@ -1,30 +1,24 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { ItemContext } from "./ItemContext"
-import * as XLSX from 'xlsx';
 import { CSVLink } from "react-csv";
 
 export default function RubricBox() {
     let {totalItems, Hierarchy} = useContext(ItemContext);
 
     return(
-          <div className="rubricBox">
+          <div className="rubricBoxContainer">
+            <div className='rubricBox'>
               <h3>Available Categories</h3>
               {Hierarchy.map((item, i) =>
               <div key={i}>{item.returnHTML()}</div>)}
-              {/*<a href="#">
-                <button className="generalButton" 
-                        style={{float:"right"}} 
-                        onClick={handleExportButtonClick}>
-                  Export
-                </button>
-              </a>*/}
-              <CSVLink 
-                data={totalItems}
-                filename={"Export_Results.csv"}
-                className='csvLink'>
-                Export
-              </CSVLink>
-
+            </div>
+            {/* Export button */}
+            <CSVLink 
+              data={totalItems}
+              filename={"Export_Results.csv"}
+              className='csvLink'>
+              Export
+            </CSVLink>
           </div>
     )
 }
