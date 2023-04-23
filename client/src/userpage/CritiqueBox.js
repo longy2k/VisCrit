abda0a587efa985fb9e336414d0fed6e3e1358eb
seyelence <infinityx14@gmail.com>
2023-04-23 08:35:20 -0400

    added likertval to be safed

--- client/src/userpage/CritiqueBox.js
+++ client/src/userpage/CritiqueBox.js
@@ -10,11 +10,11 @@ export default function CritiqueBox(){
         return (
             <div className="critiqueBox" >
                 <button style={{'visibility':`${index === -1 ? 'hidden' : 'visible'}` }} className="generalButton" onClick={() => {setAccessCanvas(true)}}>Location</button> {/*for eventual location*/}
-                <button style={{'visibility':`${currentItem.LikertValue[0] === '' ?  'visible' : 'hidden'}` }}id="one" onClick={() => {setIndex(0)}}>1</button>
-                <button style={{'visibility':`${currentItem.LikertValue[1] === '' ?  'visible' : 'hidden'}` }}id="two" onClick={() => {setIndex(1)}}>2</button>
-                <button style={{'visibility':`${currentItem.LikertValue[2] === '' ?  'visible' : 'hidden'}` }}id="three" onClick={() => {setIndex(2)}}>3</button>
-                <button style={{'visibility':`${currentItem.LikertValue[3] === '' ?  'visible' : 'hidden'}` }}id="four" onClick={() => {setIndex(3)}}>4</button>
-                <button style={{'visibility':`${currentItem.LikertValue[4] === '' ?  'visible' : 'hidden'}` }}id="five" onClick={() => {setIndex(4)}}>5</button>
+                <button style={{'visibility':`${currentItem.Comment[0] === '' ?  'visible' : 'hidden'}` }}id="one" onClick={() => {setIndex(0)}}>1</button>
+                <button style={{'visibility':`${currentItem.Comment[1] === '' ?  'visible' : 'hidden'}` }}id="two" onClick={() => {setIndex(1)}}>2</button>
+                <button style={{'visibility':`${currentItem.Comment[2] === '' ?  'visible' : 'hidden'}` }}id="three" onClick={() => {setIndex(2)}}>3</button>
+                <button style={{'visibility':`${currentItem.Comment[3] === '' ?  'visible' : 'hidden'}` }}id="four" onClick={() => {setIndex(3)}}>4</button>
+                <button style={{'visibility':`${currentItem.Comment[4] === '' ?  'visible' : 'hidden'}` }}id="five" onClick={() => {setIndex(4)}}>5</button>
                 <CommentBox />
             </div>
         )
