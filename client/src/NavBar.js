d109a8e56ba25374ee4c6624afe78621e7df5504
Long Huynh <longhuynh.y2k@gmail.com>
2023-05-03 09:48:59 -0400

    Remove navbar and added upload to userPage

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,85 +1,11 @@
 import React, {useState} from "react"
-import * as XLSX from 'xlsx';
-import axios from 'axios';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import UserPage from "./userpage/UserPage";
 import './assets/css/NavBar.css';
 
 export default function NavBar() {
-    const [jsonData, setJsonData] = useState(null);
-    const [fileUploaded, setFileUploaded] = useState(false);
-    const [participantsData, setParticipantsData] = useState([]);
-
-    const readUploadFile = async (e) => {
-      e.preventDefault();
-      const files = e.currentTarget.files;
-      if (files.length > 0) {
-        const file = files[0];
-        const reader = new FileReader();
-    
-        if (file.type === "application/vnd.ms-excel" || file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.type === "text/csv") {
-          reader.onload = async (e) => {
-            const data = e.target.result;
-            const workbook = XLSX.read(data, { type: "array" });
-            const sheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[sheetName];
-            const json = XLSX.utils.sheet_to_json(worksheet);
-            setJsonData(json);
-            setFileUploaded(true);
-            console.log(json);
-    
-            // Upload JSON file to server
-            const formData = new FormData();
-            formData.append('file', new Blob([JSON.stringify(json, null, 2)], { type: 'application/json' }), file.name.replace(/\.[^/.]+$/, ".json"));
-            try {
-              const response = await axios.post('/api/upload/', formData);
-              console.log(response.data);
-            } catch (error) {
-              console.error(error);
-            }
-    
-            const participantsSheetName = workbook.SheetNames[3];
-            const participantsWorksheet = workbook.Sheets[participantsSheetName];
-            const jsonData = XLSX.utils.sheet_to_json(participantsWorksheet);
-            setParticipantsData(jsonData);
-            window.location.reload();
-          };
-          reader.readAsArrayBuffer(file);
-        } else if (file.type === "application/pdf" || file.type === "image/jpeg" || file.type === "image/png") {
-          // Upload file to server
-          const formData = new FormData();
-          formData.append('file', file, file.name);
-          try {
-            const response = await axios.post('/api/upload/', formData);
-            console.log(response.data);
-            window.location.reload();
-          } catch (error) {
-            console.error(error);
-          }
-        } else {
-          console.error("Invalid file type.");
-        }
-      }
-    };
-    
-
-    const handleUploadButtonClick = (e) => {
-      const uploadInput = document.createElement('input');
-      uploadInput.type = 'file';
-      uploadInput.accept = '.xlsx, .xls, .csv, .pdf, .jpeg, .png';
-      uploadInput.multiple = false;
-      uploadInput.onchange = readUploadFile;
-      uploadInput.click();
-    }
-
     return (
         <Router>
-          <div className="navBar">
-            <ul>
-                <li><Link to="/"><span id="logo">VisCrit</span></Link></li>
-                <li><button className="uploadButton" onClick={handleUploadButtonClick}>Upload</button></li>
-            </ul>
-          </div>
           <Routes>
               <Route exact path='/' element={< UserPage />}></Route>
           </Routes>
--- client/src/userpage/UserPage.js
+++ client/src/userpage/UserPage.js
@@ -4,6 +4,8 @@ import Data_Extractor from "./Data_Extract";
 import DocumentReader from "./DocumentReader";
 import { ItemContext } from "./ItemContext";
 import "../assets/css/UserPage.css";
+import * as XLSX from 'xlsx';
+import axios from 'axios';
 
 export default function UserPage() {
   const [currentItem, setItem] = useState({});
@@ -15,6 +17,71 @@ export default function UserPage() {
   const [index, setIndex] = useState(-1);
   const [totalItems, setTotalItems] = useState([]);
   const [directoryExists, setDirectoryExists] = useState(false);
+  const [jsonData, setJsonData] = useState(null);
+  const [fileUploaded, setFileUploaded] = useState(false);
+  const [participantsData, setParticipantsData] = useState([]);
+
+  const readUploadFile = async (e) => {
+    e.preventDefault();
+    const files = e.currentTarget.files;
+    if (files.length > 0) {
+      const file = files[0];
+      const reader = new FileReader();
+  
+      if (file.type === "application/vnd.ms-excel" || file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.type === "text/csv") {
+        reader.onload = async (e) => {
+          const data = e.target.result;
+          const workbook = XLSX.read(data, { type: "array" });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const json = XLSX.utils.sheet_to_json(worksheet);
+          setJsonData(json);
+          setFileUploaded(true);
+          console.log(json);
+  
+          // Upload JSON file to server
+          const formData = new FormData();
+          formData.append('file', new Blob([JSON.stringify(json, null, 2)], { type: 'application/json' }), file.name.replace(/\.[^/.]+$/, ".json"));
+          try {
+            const response = await axios.post('/api/upload/', formData);
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+  
+          const participantsSheetName = workbook.SheetNames[3];
+          const participantsWorksheet = workbook.Sheets[participantsSheetName];
+          const jsonData = XLSX.utils.sheet_to_json(participantsWorksheet);
+          setParticipantsData(jsonData);
+          window.location.reload();
+        };
+        reader.readAsArrayBuffer(file);
+      } else if (file.type === "application/pdf" || file.type === "image/jpeg" || file.type === "image/png") {
+        // Upload file to server
+        const formData = new FormData();
+        formData.append('file', file, file.name);
+        try {
+          const response = await axios.post('/api/upload/', formData);
+          console.log(response.data);
+          window.location.reload();
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        console.error("Invalid file type.");
+      }
+    }
+  };
+  
+
+  const handleUploadButtonClick = (e) => {
+    const uploadInput = document.createElement('input');
+    uploadInput.type = 'file';
+    uploadInput.accept = '.xlsx, .xls, .csv, .pdf, .jpeg, .png';
+    uploadInput.multiple = false;
+    uploadInput.onchange = readUploadFile;
+    uploadInput.click();
+  }
 
   useEffect(() => {
     fetch('/api/upload/json')
@@ -45,6 +112,7 @@ export default function UserPage() {
       <div className="directoryNotFound">
         <h1 className="noUploadViscrit">VISCRIT</h1>
         <p className="noUploadText">Please upload your files.</p>
+        <button className="uploadButton" onClick={handleUploadButtonClick}>Upload</button>
       </div>
     );
   }
