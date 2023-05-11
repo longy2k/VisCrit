e96e84ae228be90e3b79792e8c6c272cdf4e30f0
seyelence <infinityx14@gmail.com>
2023-05-11 14:46:36 -0400

    not done

--- client/src/userpage/CritiqueBox.js
+++ client/src/userpage/CritiqueBox.js
@@ -6,7 +6,8 @@ import ExportItem from './ExportItem';
 export default function CritiqueBox(){
     const [comment, setComment] = useState("");
     let {currentItem, setAccessCanvas, index, setIndex, rectangles, pageNumber, locked, critiquerID, totalItems, setItem} = useContext(ItemContext);
-    
+    const [selectedButtonIndex, setSelectedButtonIndex] = useState(-1);
+
     function RemoveComment(){
         if(index === -1){
             setItem(null);
@@ -42,6 +43,7 @@ export default function CritiqueBox(){
     function IndexClick(num = -1){
         setIndex(num);
         setComment(currentItem.Comment[num]);
+        setSelectedButtonIndex(num);
     }
 
     if (currentItem instanceof Item) {
@@ -50,12 +52,23 @@ export default function CritiqueBox(){
                 <div className="critiqueBox">
                     <div style={{margin: '0', width: "20vw"}}>
                         <h4> Rating: </h4>
+
                         <div style={{display: 'flex'}}>
-                            <button  id="one" onClick={() => {IndexClick(0)}}>1</button>
-                            <button  id="two" onClick={() => {IndexClick(1)}}>2</button>
-                            <button  id="three" onClick={() => {IndexClick(2)}}>3</button>
-                            <button  id="four" onClick={() => {IndexClick(3)}}>4</button>
-                            <button  id="five" onClick={() => {IndexClick(4)}}>5</button>
+                            <button  id="one" onClick={() => {IndexClick(0)}}
+                            className={selectedButtonIndex === 0 ? 'selected' : ''}> 
+                            1</button>
+                            <button  id="two" onClick={() => {IndexClick(1)}}
+                            className={selectedButtonIndex === 1 ? 'selected' : ''}
+                            >2</button>
+                            <button  id="three" onClick={() => {IndexClick(2)}}
+                            className={selectedButtonIndex === 2 ? 'selected' : ''}
+                            >3</button>
+                            <button  id="four" onClick={() => {IndexClick(3)}}
+                            className={selectedButtonIndex === 3 ? 'selected' : ''}
+                            >4</button>
+                            <button  id="five" onClick={() => {IndexClick(4)}}
+                            className={selectedButtonIndex === 4 ? 'selected' : ''}
+                            >5</button>
                             <button style={{margin: '-3px 13px'}} className="locationButton" onClick={() => {setAccessCanvas(true)}}>Location</button>
                         </div>
                     </div>
