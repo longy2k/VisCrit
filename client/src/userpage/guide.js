10b8d73875cd05fd40242a50e7edbabf0b5b5efe
christos kakouros <christos.kakouros001@umb.edu>
2023-05-11 18:06:23 -0400

    user guide update

--- client/src/userpage/guide.js
+++ client/src/userpage/guide.js
@@ -14,28 +14,29 @@ const UserGuide = () => {
   };
 
   const getContentTransform = () => {
-    return `translateX(-${(currentPage - 1) * 36}%)`;
+    return `translateX(-${(currentPage - 1) * 20}%)`;
   };
 
   return (
-    <div className='containerGuide'>
+    <div className="user-guide-container">
+      <h1 className="user-guide-heading"></h1>
       <div className="user-guide-content" style={{ transform: getContentTransform() }}>
-        <div>
+        <div style={{opacity: currentPage === 1 ? 1 :0, tranform: getContentTransform()}}>
           <h2>Step 1: Getting Started</h2>
           <p>First, you have to upload your files</p>
           <img className='picture' src={uploadimg} alt="Step 1" />
         </div>
-        <div>
+        <div style={{opacity: currentPage === 2 ? 1 :0, tranform: getContentTransform()}}>
           <h2>Step 2: Next, you have to</h2>
           <p>Choose a critqueID and select a category</p>
           <img className='picture' src={cat} alt="Step 2" />
         </div>
-        <div>
+        <div style={{opacity: currentPage === 3 ? 1 :0, tranform: getContentTransform()}}>
           <h2>Step 3: Rating and Location</h2>
           <p>To choose a rating and a location:</p>
           <img className='picture' src={loc} alt="Step 3" />
         </div>
-        <div>
+        <div style={{opacity: currentPage === 4 ? 1 :0, tranform: getContentTransform()}}>
           <h2>Step 4: Making Annotations</h2>
           <p>After choosing a rating and location, you can now highlight the text you want and leave a comment</p>
           <img className='picture' src={comment} alt="Step 4" />
